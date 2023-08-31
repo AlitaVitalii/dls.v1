83418ee459d15938da8bc9a08294153184b53353
alita <alita.avs@gmail.com>
2023-08-31 23:36:50 +0300

    step2

--- pubdls/models.py
+++ pubdls/models.py
@@ -1,3 +1,19 @@
+from datetime import date
+
 from django.db import models
 
 # Create your models here.
+
+
+class Regnum(models.Model):
+    reg_num = models.CharField(max_length=100, verbose_name='№ документу')
+    pub_date = models.DateField(default=date.today, verbose_name='Дата додавання')
+    reg_date = models.CharField(max_length=10, verbose_name='Дата документу')
+    doc_type = models.CharField(max_length=100, verbose_name='Тип документу')
+    rp_number = models.CharField(max_length=100, verbose_name='№ РП ЛЗ')
+    drug_name = models.TextField(max_length=1000, verbose_name='Назва ЛЗ, форма випуску')
+    serial_num = models.CharField(max_length=100, verbose_name='Серія №')
+    manufacture = models.CharField(max_length=255, verbose_name='Назва виробника, країна')
+
+    def __str__(self):
+        return self.reg_num
--- pubdls/tasks.py
+++ pubdls/tasks.py
@@ -3,9 +3,11 @@ from bs4 import BeautifulSoup
 from celery import shared_task
 from django.core.mail import send_mail
 
+from pubdls.models import Regnum
+
 
 @shared_task()
-def quotes_parser():
+def parser():
     start_date = '01-01-2023'
     end_date = '04-01-2023'
     # r = requests.get("http://pub-mex.dls.gov.ua/QLA/DocList.aspx")
@@ -17,21 +19,44 @@ def quotes_parser():
         '__VIEWSTATE': '',
         '__VIEWSTATEGENERATOR': '',
         '__EVENTVALIDATION': '',
-        'ctl00_Content_fvParams_edtDocDateBegin': start_date,
-        'ctl00_Content_fvParams_edtDocDateEnd': end_date,
+        'ctl00$ContentPlaceHolder1$txtDateFrom': start_date,
+        'ctl00$ContentPlaceHolder1$txtDateTo': end_date,
         'ctl00$ContentPlaceHolder1$Button1': 'Запит'
     })
-    pub_date = []
-    numb_doc = []
-    type_doc = []
-    numb_rp = []
-    name = []
-    sert = []
-    manufacturer = []
+    # regdate_list = []
+    regnum_list = []
+    # doctype_list = []
+    # rpnumber_list = []
+    # drugname_list = []
+    # serialnum_list = []
+    # manufacture_list = []
 
     while True:
         if r.status_code == 200:
-            soup_nav = BeautifulSoup(r.text, 'html.parser')
+            soup = BeautifulSoup(r.text, 'html.parser')
+            soup_rows = soup.select('tr[class$="Row"]')
+
+            for i in range(len(soup_rows)):
+                regnum = Regnum.objects.filter(name=str(soup_rows[i].select('td[id$="RegNum"]'))[46:-6])
+                if not regnum:
+                    reg_number = Regnum.objects.create(
+                        reg_num=regnum,
+                        reg_date=str(soup_rows[i].select('td[id$="RegDate"]'))[73:-6],
+                        doc_type=str(soup_rows[i].select('td[id$="DocTypeName"]'))[51:-6],
+                        rp_number=str(soup_rows[i].select('td[id$="RpNumber"]'))[48:-6],
+                        drug_name=str(soup_rows[i].select('td[id$="DrugNameAndFormtypeDesc"]'))[63:-6],
+                        serial_num=str(soup_rows[i].select('td[id$="SerialNum"]'))[49:-6],
+                        manufacture=str(soup_rows[i].select('td[id$="ProducerAndCountry"]'))[58:-6],
+
+                    )
+                    regnum_list.append(reg_number)
+
+        print(regnum_list)
+
+
+
+
+
 
 
 
